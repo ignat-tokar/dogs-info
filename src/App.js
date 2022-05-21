2a1204cd394ff048b0ad7743f0022a85965929f7
Ignat Tokar <ignat-tokar@example.com>
2022-05-21 15:45:35 +0300

    developing of Data Acces Layer is complete

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { breedsAPI, favouritesAPI, imagesAPI, votesAPI } from './api/api';
 import './App.css';
 import DogInfo from './components/DogInfo';
 
 
 function App() {
 
-  const [dog, setDog] = useState(null);
-  const [breed, setBreed] = useState(null);
-
-  useEffect(() => {
-    axios
-      .get('https://api.thedogapi.com/v1/images/search')
-      .then((response) => {
-        setBreed(response.data[0].breeds[0]);
-        setDog(response.data[0]);
-      })
-      .catch(e => console.log(e));
-  }, []);
-
   return (
     <div className="App">
-      {(dog && breed)
-        ? <DogInfo url={dog.url} breed={breed} />
-        : 
-        <>
-          <h1>Loading, please wait ...</h1>
-          <h3>If loading wouldn't be completed about 5 seconds please reload the page.</h3>
-        </>
-      }
+      <h1>Soon here would be UI.</h1>
     </div>
   );
 }
---
+++ src/api/api.js
@@ -0,0 +1,41 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+  // withCredentials: true,
+  baseURL: 'https://api.thedogapi.com/v1/',
+  headers: {
+    "x-api-key": "8ec182bb-2325-41d5-ba4b-82fbb2f961e5"
+  }
+});
+
+const instanceGet = path => instance.get(path).then(response => response.data);
+
+const instancePost = (path, data) => instance.post(path, data)
+  .then(response => response.data);
+
+const instanceDelete = path => instance.delete(path).then(response => response.data);
+
+export const breedsAPI = {
+  getBreeds: (page = 0, limit = 10) => 
+    instanceGet(`breeds?limit=${limit}&page=${page}`),
+  getBreedById: (breed_id = 2) => instanceGet(`breeds/${breed_id}`)
+}
+
+export const imagesAPI = {
+  getImageById: id => instanceGet(`images/${id}`),
+  getBreedInfoByImageId: id => instanceGet(`images/${id}/breeds`)
+}
+
+export const favouritesAPI = {
+  getFavourites: () => instanceGet('favourites'),
+  postFavourites: image_id => instancePost('favourites', {image_id}),
+  deleteFavourite: favourite_id => instanceDelete(`favourites/${favourite_id}`)
+}
+
+export const votesAPI = {
+  getVotes: () => instanceGet('votes'),
+  // Off post-process for votes, because deleting not working.
+  // postVotes: image_id => instancePost('votes', {image_id, value: 0}),
+  // Don't work deleting of votes.
+  // deleteVote: vote_id => instanceDelete(`votes/${vote_id}`)
+}
